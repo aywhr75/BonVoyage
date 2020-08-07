f8213ccbcffe4f6d60e00fcd21ae7c47190694f2
young <aywhr75@gmail.com>
2020-08-07 12:46:47 -0400

    heroku connect1

---
+++ client/build/precache-manifest.793e2b40528ff4b57a83100d1cb79d8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86169f4758d170f6382b333a126613f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "451eea8a87a011b76cb2",
+    "url": "/static/css/2.a1251ad2.chunk.css"
+  },
+  {
+    "revision": "339c1f3ef0764e81b703",
+    "url": "/static/css/main.98d21785.chunk.css"
+  },
+  {
+    "revision": "451eea8a87a011b76cb2",
+    "url": "/static/js/2.dea8319b.chunk.js"
+  },
+  {
+    "revision": "c0554f266784fabb0402ff1139c47fda",
+    "url": "/static/js/2.dea8319b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "339c1f3ef0764e81b703",
+    "url": "/static/js/main.ee44dffe.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  }
+]);
